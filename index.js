fcd220ae01d969aa08c5cb5029faef1213f7bc25
Densel <densel.esekon2@student.moringaschool.com>
2023-09-18 20:12:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+/* describe('employees', function() {
+    describe('updateEmployeeWithKeyAndValue(employee, key, value)', function () {
+        beforeEach(function () {
+          for (const key in employee) {
+            delete employee[key];
+          }
+    
+          employee.name = 'Sam';
+        }); */
+const employee = {}  
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {    
+    const updatedEmployee = { ...employee };        
+    updatedEmployee[key] = value;      
+    return updatedEmployee;
+  }  
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {    
+    employee[key] = value;    
+    return employee;
+  }
+  
+function deleteFromEmployeeByKey(employee, key) {    
+    const updatedEmployee = { ...employee };    
+    delete updatedEmployee[key];    
+    return updatedEmployee;
+  }
+  
+function destructivelyDeleteFromEmployeeByKey(employee, key) {    
+    delete employee[key];   
+    return employee;
+  }
+  
